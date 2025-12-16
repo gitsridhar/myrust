2906bcbd860158e0ce88c40a5cfaaf09d685f1c2
Sridhar Venkat <svenkat@us.ibm.com>
2025-12-16 14:12:28 -0500

    Additional changes
    Signed-off-by: Sridhar Venkat <svenkat@us.ibm.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-	println!("Hello, world!");
+    println!("This is a fourth main ! {} " , gcd(10,5))
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 54321, 5 * 7 * 54321), 54321);
+
+    let mut x;
+    x = 42;
+    let y = &mut x;
+    //x = 43;
+    *y = 43;
+    assert_eq!(*y, 43);
+    assert_eq!(x, 43);
 }
